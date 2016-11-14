19596ccdcc65bb071718276e9d6f9440f8d93fd6
hankovich <svyatoslav.hankovich@gmail.com>
2016-11-14 23:32:34 +0300

    Tests, xml-comments and other shapes added.

---
+++ Task2.Logic.Tests/ShapeTests.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task2.Logic.Tests
+{
+    [TestFixture]
+    public class TriangleTests
+    {
+        [TestCase(1, 1, 1)]
+        [Test]
+        public void EpsAccuracy(double a, double b, double c)
+        {
+            var triangle = new Triangle(b, c, a);
+            Assert.LessOrEqual(Math.Abs(a - triangle.A), Shape.Eps);
+        }
+
+        [TestCase(-1, 1, 1, typeof (ArgumentOutOfRangeException))]
+        [TestCase(10, 10, 300000, typeof (ArgumentException))]
+        [TestCase(1, 100, 50, typeof (ArgumentException))]
+        [Test]
+        public void ExceptionExpected(double a, double b, double c, Type exceptionType)
+        {
+            Assert.Throws(exceptionType, () => new Triangle(b, c, a));
+        }
+
+        [TestCase(Math.E, Math.E, Math.E, Math.E*3)]
+        [Test]
+        public void Perimeter(double a, double b, double c, double expectedPerimeter)
+        {
+            Triangle triangle = new Triangle(b, c, a);
+            Assert.LessOrEqual(Math.Abs(expectedPerimeter - triangle.Perimeter), Shape.Eps);
+        }
+
+        [TestCase(3, 4, 5, 6)]
+        [TestCase(Math.E, Math.E, Math.E, Math.E*Math.E*1.732050807/4.0)]
+        [Test]
+        public void Area(double a, double b, double c, double expectedArea)
+        {
+            Triangle triangle = new Triangle(b, c, a);
+            Assert.LessOrEqual(Math.Abs(expectedArea - triangle.Area), Shape.Eps);
+        }
+    }
+
+
+    [TestFixture]
+    public class CircleTests
+    {
+        [TestCase(1)]
+        [Test]
+        public void EpsAccuracy(double radius)
+        {
+            var circle = new Circle(radius);
+            Assert.LessOrEqual(Math.Abs(radius - circle.Radius), Shape.Eps);
+        }
+
+        [TestCase(-1, typeof (ArgumentOutOfRangeException))]
+        [TestCase(double.Epsilon/2.0, ExpectedResult = typeof (ArgumentOutOfRangeException))]
+        [Test]
+        public void ExceptionExpected(double radius, Type exceptionType)
+        {
+            Assert.Throws(exceptionType, () => new Circle(radius));
+        }
+
+        [TestCase(Math.E, Math.E*Math.PI*2)]
+        [Test]
+        public void Perimeter(double radius, double expectedPerimeter)
+        {
+            Circle circle = new Circle(radius);
+            Assert.LessOrEqual(Math.Abs(expectedPerimeter - circle.Perimeter), Shape.Eps);
+        }
+
+        [TestCase(1, Math.PI)]
+        [TestCase(Math.E, Math.E*Math.E*Math.PI)]
+        [Test]
+        public void Area(double radius, double expectedArea)
+        {
+            Circle circle = new Circle(radius);
+            Assert.LessOrEqual(Math.Abs(expectedArea - circle.Area), Shape.Eps);
+        }
+    }
+
+    [TestFixture]
+    public class RectangleTests
+    {
+        [TestCase(1, 2)]
+        [Test]
+        public void EpsAccuracy(double a, double b)
+        {
+            var rectangle = new Rectangle(a, b);
+            Assert.LessOrEqual(Math.Abs(a - rectangle.A), Shape.Eps);
+        }
+
+        [TestCase(-1, 1, typeof (ArgumentOutOfRangeException))]
+        [Test]
+        public void ExceptionExpected(double a, double b, Type exceptionType)
+        {
+            Assert.Throws(exceptionType, () => new Rectangle(a, b));
+        }
+
+        [TestCase(Math.E, Math.PI, (Math.PI + Math.E)*2)]
+        [Test]
+        public void Perimeter(double a, double b, double expectedPerimeter)
+        {
+            Rectangle rectangle = new Rectangle(a, b);
+            Assert.LessOrEqual(Math.Abs(expectedPerimeter - rectangle.Perimeter), Shape.Eps);
+        }
+
+        [TestCase(1, 1, 1)]
+        [TestCase(Math.E, Math.PI, Math.PI*Math.E)]
+        [Test]
+        public void Area(double a, double b, double expectedArea)
+        {
+            Rectangle rectangle = new Rectangle(a, b);
+            Assert.LessOrEqual(Math.Abs(expectedArea - rectangle.Area), Shape.Eps);
+        }
+    }
+
+    [TestFixture]
+    public class SquareTests
+    {
+        [TestCase(Math.PI)]
+        [Test]
+        public void EpsAccuracy(double a)
+        {
+            var square = new Square(a);
+            Assert.LessOrEqual(Math.Abs(a - square.A), Shape.Eps);
+        }
+
+        [TestCase(-11, typeof(ArgumentOutOfRangeException))]
+        [Test]
+        public void ExceptionExpected(double a, Type exceptionType)
+        {
+            Assert.Throws(exceptionType, () => new Square(a));
+        }
+
+        [TestCase(Math.E, Math.PI, Math.E * 4)]
+        [Test]
+        public void Perimeter(double a, double b, double expectedPerimeter)
+        {
+            Rectangle square = new Square(a);
+            Assert.LessOrEqual(Math.Abs(expectedPerimeter - square.Perimeter), Shape.Eps);
+        }
+
+        [TestCase(1, 1)]
+        [TestCase(Math.E, Math.E * Math.E)]
+        [Test]
+        public void Area(double a, double expectedArea)
+        {
+            Rectangle square = new Square(a);
+            Assert.LessOrEqual(Math.Abs(expectedArea - square.Area), Shape.Eps);
+        }
+    }
+}
--- Task2.Logic/Shape.cs
+++ Task2.Logic/Shape.cs
@@ -9,6 +9,9 @@ namespace Task2.Logic
 {
     public abstract class Shape
     {
+        /// <summary>
+        /// Accuracy
+        /// </summary>
         public static double Eps { get; }
 
         static Shape()
@@ -23,7 +26,7 @@ namespace Task2.Logic
             }
             else
             {
-                Eps = double.Epsilon;
+                Eps = 1e-6;
             }
         }
 
@@ -35,13 +38,17 @@ namespace Task2.Logic
     {
         private double radius;
 
+        /// <summary>
+        /// Radius of a circle
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">Value is less than Eps</exception>
         public double Radius
         {
             get { return radius; }
             set
             {
                 if (value >= Eps)
-                    Radius = value;
+                    radius = value;
                 else
                 {
                     throw new ArgumentOutOfRangeException($"Check {nameof(value)}. It must be greater than {Eps}");
@@ -54,24 +61,163 @@ namespace Task2.Logic
             Radius = r;
         }
 
+        /// <summary>
+        /// Count circle perimeter
+        /// </summary>
         public override double Perimeter => 2*Math.PI*Radius;
 
+        /// <summary>
+        /// Count circle area
+        /// </summary>
         public override double Area => Math.PI*Radius*Radius;
     }
 
     public class Triangle : Shape
     {
-        
+        /// <summary>
+        /// Length of first triangle side
+        /// </summary>
+        public double A { get; }
+
+        /// <summary>
+        /// Length of second triangle side
+        /// </summary>
+        public double B { get; }
+
+        /// <summary>
+        /// Length of third triangle side
+        /// </summary>
+        public double C { get; }
+
+        public Triangle(double y, double z, double x)
+        {
+            if (x < Eps)
+                throw new ArgumentOutOfRangeException($"Check {nameof(x)}. It must be greater than {Eps}.");
+            if (y < Eps)
+                throw new ArgumentOutOfRangeException($"Check {nameof(y)}. It must be greater than {Eps}.");
+            if (z < Eps)
+                throw new ArgumentOutOfRangeException($"Check {nameof(z)}. It must be greater than {Eps}.");
+
+            if (!isValid(x, y, z))
+                throw new ArgumentException(
+                    $"Check {nameof(x)}, {nameof(y)} and {nameof(z)}. Don't forget the triangle inequality.");
+            
+            A = x;
+            B = y;
+            C = z;
+        }
+
+        /// <summary>
+        /// Perimeter of a triangle
+        /// </summary>
+        public override double Perimeter => A + B + C;
+
+        /// <summary>
+        /// Area of a triangle
+        /// </summary>
+        public override double Area => Math.Sqrt(Perimeter*(Perimeter - 2*A)*(Perimeter - 2*B)*(Perimeter - 2*C))/4.0;
+
+        private bool isValid(double a, double b, double c)
+        {
+            if (a + b - c > Eps && a + c - b > Eps && b + c - a > Eps)
+                return true;
+            return false;
+        }
     }
 
     public class Rectangle : Shape
     {
-        
+        protected double a;
+        protected double b;
+
+
+        /// <summary>
+        /// Length of first rectangle side
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">Value is less than Eps</exception>
+        public double A
+        {
+            get { return a; }
+            set
+            {
+                if (value >= Eps)
+                    a = value;
+                else
+                {
+                    throw new ArgumentOutOfRangeException($"Check {nameof(value)}. It must be greater than {Eps}.");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Length of second rectangle side
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">Value is less than Eps</exception>
+        public double B
+        {
+            get { return b; }
+            set
+            {
+                if (value >= Eps)
+                    b = value;
+                else
+                {
+                    throw new ArgumentOutOfRangeException($"Check {nameof(value)}. It must be greater than {Eps}.");
+                }
+            }
+        }
+
+        public Rectangle(double x, double y)
+        {
+            A = x;
+            B = y;
+        }
+        /// <summary>
+        /// Perimeter of a rectangle
+        /// </summary>
+        public override double Perimeter => 2*(A + B);
+
+        /// <summary>
+        /// Area of a rectangle
+        /// </summary>
+        public override double Area => A*B;
     }
 
-    public class Square : Rectangle
+    public class Square : Rectangle //It's not perfect, but I hope that it's better than inheritance from a shape. Maybe Polygon will solve this problem
     {
-        
-    }
+        /// <summary>
+        /// Length of square side 
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">Value is less than Eps</exception>
+        public double A
+        {
+            get { return a; }
+            set
+            {
+                if (value >= Eps)
+                {
+                    a = value;
+                    b = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException($"Check {nameof(value)}. It must be greater than {Eps}.");
+                }
+            }
+        }
 
+        protected double B;
+
+        public Square(double x) : base(x, x) {}
+
+        /// <summary>
+        /// Perimeter of a square
+        /// </summary>
+        public override double Perimeter => 4 * A;
+
+        /// <summary>
+        /// Area of a square
+        /// </summary>
+        public override double Area => A * A;
+    }
 }
